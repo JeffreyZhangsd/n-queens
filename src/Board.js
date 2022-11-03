13a089fce88f43a293437d9187da8f3a0dca9cb9
Jeffrey Zhang <jzhang22la@gmail.com>
2022-11-02 17:53:48 -0700

    completed board.js conflict functions and started whiteboarding for solvers.js

--- src/Board.js
+++ src/Board.js
@@ -62,7 +62,7 @@
     },
 
 
-/*
+ /*
          _             _     _
      ___| |_ __ _ _ __| |_  | |__   ___ _ __ ___ _
     / __| __/ _` | '__| __| | '_ \ / _ \ '__/ _ (_)
@@ -78,57 +78,197 @@
     // --------------------------------------------------------------
     //
     // test if a specific row on this board contains a conflict
+
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      //inputs: rowIndex, return a boolean
+      //chessboard.hasRowConflictAt(row1)
+      //loop through row
+        //if there are multiple 1's
+          //return true
+      //otherwise return false
+      //"this" refers to board
+      var row = this.get(rowIndex);
+      var count = 0;
+      for (var i = 0; i < row.length; i++) {
+        if (row[i] === 1) {
+          count++;
+        }
+        if (count > 1) {
+          return true;
+        }
+      }
+      return false;
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      //iterate for each column
+        //use hasRowConflictAt for each column
+      var rowArray = this.rows()
+      for (var i = 0; i < rowArray.length; i++) {
+        //checking each row
+        if (this.hasRowConflictAt(i)) {
+          return true;
+        }
+      }
+      return false; //
     },
 
 
+    /*
+    [ c1 c2 c3 c4
+      [0, 0, 0, 0], row1 [0]
+      [0, 0, 0, 0], row2 [1]
+      [0, 0, 0, 0], row3 [2]
+      [0, 0, 0, 0]  row4 [3]
+    ]
+    */
+    //backbone things   this.get(rowIndex)[colIndex]
+    // var someBoardName = new Board({ n:4 })
+    // someBoardName.togglePiece(rowIndex, colIndex);
+
+    // togglePiece(0, 0);
+    // togglePiece(0, 1);
+    // [1, 1, 0, 0] row 1
+    // someBoardName.board.hasRowConflictAt(row1)
 
     // COLUMNS - run from top to bottom
     // --------------------------------------------------------------
     //
     // test if a specific column on this board contains a conflict
-    hasColConflictAt: function(colIndex) {
+    hasColConflictAt: function(colIndex) { //(row index) (col index)
+      //get our rows using this.rows
+      //for loop through this.rows
+        //var columnValue = this.get(i)[column index]
+        //if (columnValue === 1) {count++}
+      var rowsArray = this.rows();
+      var count = 0;
+      for (var i = 0; i < rowsArray.length; i++) {
+        if (this.get(i)[colIndex]) {
+          count++;
+        }
+        if (count > 1) {
+          return true;
+        }
+      }
       return false; // fixme
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
+      //same logic as rows since the board is n x n
+      var rowsArray = this.rows();
+      for (var i = 0; i < rowsArray.length; i++) {
+        if (this.hasColConflictAt(i)) {
+          return true;
+        }
+      }
       return false; // fixme
     },
 
 
 
+    /*
+    [ c1 c2 c3 c4
+      [0, 0, 0, 0], row1 [0]
+      [0, 0, 0, 0], row2 [1]
+      [0, 0, 0, 0], row3 [2]      MVP
+      [0, 0, 0, 1]  row4 [3]
+    ]
+    */
+    //backbone things   this.get(rowIndex)[colIndex]
+    // var someBoardName = new Board({ n:4 })
+    // someBoardName.togglePiece(rowIndex, colIndex);
+
+    // togglePiece(0, 0);
+    // togglePiece(0, 1);
+    // [1, 1, 0, 0] row 1
+    // someBoardName.board.hasRowConflictAt(row1)
     // Major Diagonals - go from top-left to bottom-right
     // --------------------------------------------------------------
-    //
+    // (+1, +1)
+
     // test if a specific major diagonal on this board contains a conflict
-    hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
+    hasMajorDiagonalConflictAt: function(row, column) {
+      //if index of (+1, +1) exists
+        //check if index of (+1, +1) is 1 ---- recursive call of if statement above
+      var rowArray = this.rows();
+      for (var diag = 1; diag < rowArray.length - 1; diag++) {
+        if (rowArray[row + diag] !== undefined && rowArray[row + diag][column + diag] !== undefined) {
+          if (rowArray[row + diag][column + diag] === 1) {
+            return true;
+          }
+        }
+      }
       return false; // fixme
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
+      //working on this first to check our inputs
+      //iterate through array of rows
+        //iterate through first row n times
+          //call hasMajorDiagonalConflictAt(n)
+      var rowArray = this.rows();
+      for (var i = 0; i < rowArray.length; i++) {
+        for (var j = 0; j < rowArray[i].length; j++) {
+          if (rowArray[i][j] === 1) {
+            if (this.hasMajorDiagonalConflictAt(i, j)) {
+              return true;
+            }
+          }
+        }
+      }
       return false; // fixme
     },
 
 
+    /*
+    [ c1 c2 c3 c4
+      [0, 0, 1, 0], row1 [0]
+      [0, 0, 0, 0], row2 [1]
+      [1, 0, 0, 0], row3 [2]      MVP
+      [0, 0, 0, 0]  row4 [3]
+    ]
+    */
+    //backbone things   this.get(rowIndex)[colIndex]
+    // var someBoardName = new Board({ n:4 })
+    // someBoardName.togglePiece(rowIndex, colIndex);
 
+    // togglePiece(0, 0);
+    // togglePiece(0, 1);
+    // [1, 1, 0, 0] row 1
+    // someBoardName.board.hasRowConflictAt(row1)
     // Minor Diagonals - go from top-right to bottom-left
     // --------------------------------------------------------------
-    //
+    //(+1, -1)
     // test if a specific minor diagonal on this board contains a conflict
-    hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
+    hasMinorDiagonalConflictAt: function(row, column) {
+      //same logic as major, but we will be subtracting by diagonal instead
+      var rowArray = this.rows();
+      for (var diag = 1; diag < rowArray.length; diag++) {
+        if (rowArray[row + diag] !== undefined && rowArray[row + diag][column - diag] !== undefined) {
+          if (rowArray[row + diag][column - diag] === 1) {
+            return true;
+          }
+        }
+      }
       return false; // fixme
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
+      //looking for indexes where there are 1's
+      var rowArray = this.rows();
+      for (var i = 0; i < rowArray.length; i++) {
+        for (var j = 0; j < rowArray[i].length; j++) {
+          if (rowArray[i][j] === 1) {
+            if (this.hasMinorDiagonalConflictAt(i, j)) {
+              return true;
+            }
+          }
+        }
+      }
       return false; // fixme
     }
 
@@ -144,5 +284,50 @@
       });
     });
   };
+ }());
 
-}());
+ /*
+ n x n chessboard made up of 4 arrays with 4 elements
+ [
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0]
+ ]
+ chessboard[row][column]
+
+ specific rows
+  iterate thru row...
+  for each index
+    if index is 0, continue checking next index
+    otherwise if index is 1,
+      for each other index...
+      if index is 0, continue
+      otherwise return true
+
+ specific columns
+  to check a single column
+  first column: chessboard[rows][0]
+  second: chessboard[rows][1]
+  ...
+  last column: chessboard[rows][n]
+
+
+ \
+ \
+  \
+   \
+    \>
+
+    specific major diag --- top-left to bottom-right  \
+  checking chessboard[0][0] with chessboard[1][1]
+  or chessboard[rowIndex][colIndex] --> chessboard[rowIndex + 1][colIndex + 1]
+
+     /
+    /
+   /
+  /
+ </
+ specific minor diag --- top-right to bottom-left  /
+  checking chessboard[0][n] with chessboard[1][n - 1]
+ */
--- src/solvers.js
+++ src/solvers.js
@@ -1,8 +1,8 @@
 /*           _
-   ___  ___ | |_   _____ _ __ ___
-  / __|/ _ \| \ \ / / _ \ '__/ __|
-  \__ \ (_) | |\ V /  __/ |  \__ \
-  |___/\___/|_| \_/ \___|_|  |___/
+  ___  ___ | |_   _____ _ __ ___
+ / __|/ _ \| \ \ / / _ \ '__/ __|
+ \__ \ (_) | |\ V /  __/ |  \__ \
+ |___/\___/|_| \_/ \___|_|  |___/
 
 */
 
@@ -10,38 +10,63 @@
 // (There are also optimizations that will allow you to skip a lot of the dead search space)
 // take a look at solversSpec.js to see what the tests are expecting
 
-
+// [0, 0, 1, 0],
+// [0, 0, 0, 0],
+// [1, 0, 0, 0],
+// [0, 0, 0, 0]
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
+/*IOCE ====================================
+input: n, for an n x n matrix
+output: an n x n matrix of n rooks with no conflicts
 
+*/
 
 window.findNRooksSolution = function(n) {
   var solution = undefined; //fixme
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
-};
+ };
 
-// return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
-window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+ // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
+ window.countNRooksSolutions = function(n) {
+  var solutionCount = ; //something like n(n-1)(n-2)
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
-};
+ };
 
-// return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
-window.findNQueensSolution = function(n) {
+ // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
+ window.findNQueensSolution = function(n) {
   var solution = undefined; //fixme
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
-};
+ };
 
-// return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
-window.countNQueensSolutions = function(n) {
+ // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
+ window.countNQueensSolutions = function(n) {
   var solutionCount = undefined; //fixme
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
-};
+ };
+
+
+ /*
+ return array of arrays for given n
+ for one rook solution
+  MVP...
+  starting from [0][0]... place a rook
+    iterating thru the row, we place a rook at every [0][0 ... n]
+      since we know rooks can't be on the same row we don't have to place a rook on every index in a row
+  actually I think the MVP would be to start with [0][0], iterate thru every index and for every index place a rook ->
+    if there is a row conflict or column conflict remove the rook and go to the next index
+      so it will go thru each row, each column from [0][0] to [n][n]
+    return at the end
+    would only give us one solution basically
+
+ for multiple solutions we need to create trees to branch into other ways for every single valid move
+  starting from [0][0] gives us one solution, we should have n solutions for every way you can place a rook in the first row
+ */
